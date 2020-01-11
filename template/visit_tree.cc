35594ed518098244aa4a024e59f8ab1fdde1f977
byxiangfei <zhangxiangbupt@163.com>
2020-01-11 19:24:16 +0800

    update

--- template/visit_tree.cc
+++ template/visit_tree.cc
@@ -23,27 +23,6 @@ TreeNode* buildTree() {
 	return root;
 }
 
-
-// root , left, right
-void PreOrderIteration(TreeNode* root) {
-	if(!root) return;
-	stack<TreeNode*> s;
-	TreeNode* cur = root;
-	while( cur!= NULL || !s.empty()) {
-		if( cur!= NULL ) {
-			cout<<cur->val<<" "; //visited
-			s.push(cur);
-			cur = cur->left;
-		} else {            // !s.empty()
-			cur = s.top();
-			s.pop();
-			cur = cur->right;
-		}
-	}
-	cout<<endl;
-	return;
-}
-
 // left, root, right
 void pushLeft(stack<TreeNode*> &s, TreeNode* n) {
 	while(n != nullptr) {
@@ -51,14 +30,33 @@ void pushLeft(stack<TreeNode*> &s, TreeNode* n) {
 		n = n->left;
 	}
 }
+void pushLeftWithPrint(stack<TreeNode*> &s, TreeNode* n) {
+	while(n != nullptr) {
+		cout<<n->val<<" ";
+		s.push(n);
+		n = n->left;
+	}
+}
+
+void PreOrderIteration_0(TreeNode* root) {
+	if(!root) return;
+	stack<TreeNode*> s;
+	pushLeftWithPrint(s, root);
+	while(!s.empty()) {
+		auto cur = s.top(); s.pop();
+		pushLeftWithPrint(s, cur->right);
+	}
+	cout<<endl;
+	return;
+}
 
 void MidOrderIteration_0(TreeNode* root) {
 	if(!root) return;
 	stack<TreeNode*> s;
 	pushLeft(s, root);
 	while(!s.empty()) {
-		auto cur = s.top(); s.pop();
-		cout<<cur->val<<" ";
+		auto cur = s.top();
+		s.pop(); cout<<cur->val<<" ";
 		pushLeft(s, cur->right);
 	}
 	cout<<endl;
@@ -71,71 +69,23 @@ void PostOrderIteration_0(TreeNode* root) {
 	pushLeft(s, root);
 	TreeNode* pre = NULL;
 	while(!s.empty()) {
-		TreeNode* top = s.top();   // must have a new one
+		TreeNode* cur = s.top();   // must have a new one
 		//当right没有，或者right已经visit过了，才visit top
-		if (top->right != nullptr && top->right != pre) {
-			pushLeft(s, top->right);
+		if (cur->right != nullptr && cur->right != pre) {
+			pushLeft(s, cur->right);
 			continue;
 		}
 		s.pop();
-		cout<<top->val<<" ";
-		pre = top;
+		cout<<cur->val<<" ";
+		pre = cur;
 	}
 	cout<<endl;
 	return;
 }
-
-
-void MidOrderIteration(TreeNode* root) {
-	if(!root) return;
-	stack<TreeNode*> s;
-	TreeNode* cur = root;
-	while( cur || !s.empty()) {
-		if( cur ) {
-			s.push(cur);
-			cur = cur->left;
-		} else {
-			cur = s.top();
-			s.pop();
-			cout<<cur->val<<" ";
-			cur = cur->right;
-		}
-	}
-	cout<<endl;
-	return;
-}
-
-// left, right, root,  o(n) time & space
-// 需要一个pre记录是否visit过
-void PostOrderIteration(TreeNode* root) {
-	if(!root) return;
-	stack<TreeNode*> s;
-	TreeNode* cur = root;
-	TreeNode* pre = NULL;
-	while( cur || !s.empty()) {
-		if(cur) {
-			s.push(cur);
-			cur = cur->left;
-		} else {
-			TreeNode* top = s.top();   // must have a new one
-			//当right没有，或者right已经visit过了，才visit top
-			if(top->right == NULL || top->right == pre) {
-				s.pop();
-				cout<<top->val<<" ";
-				pre = top;
-			} else {
-				cur = top->right;
-			}
-		}
-	}
-	cout<<endl;
-	return;
-}
-
 
 int main() {
 	TreeNode* root = buildTree();
-	cout<<"pre  order: "; PreOrderIteration(root);
+	cout<<"pre  order: "; PreOrderIteration_0(root);
 	cout<<"mid  order: "; MidOrderIteration_0(root);
 	cout<<"post order: "; PostOrderIteration_0(root);
 	return 0;
