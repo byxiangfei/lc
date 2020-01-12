465aa16d183c6c48131ef2a364d7d2c44b17b96c
byxiangfei <zhangxiangbupt@163.com>
2020-01-12 15:39:02 +0800

    update

--- array/215.kth_largest_element_in_array.cc
+++ array/215.kth_largest_element_in_array.cc
@@ -6,27 +6,27 @@ using namespace std;
 // http://blog.csdn.net/u010900754/article/details/55250442
 
 int partion(vector<int> &nums, int beg, int end) {
-	int pivot = nums[beg];
-	int last = beg;
+    int pivot = nums[ beg ];
+    int last  = beg;
 
-	for(int i = beg + 1; i <= end; i++) {
-		if(nums[i] <= pivot) {
-			swap(nums[i], nums[++last]);
-		}
-	}
-	swap(nums[last], nums[beg]);
-	return last;
+    for (int i = beg + 1; i <= end; i++) {
+        if (nums[ i ] <= pivot) {
+            swap(nums[ i ], nums[ ++last ]);
+        }
+    }
+    swap(nums[ last ], nums[ beg ]);
+    return last;
 }
 
 int findKthLargest(vector<int> &nums, int k) {
-	//sort(nums.begin(), nums.end(), greater<int>());
-	//return nums[k-1];
-		int low = 0, high = nums.size() - 1;
-    while(low <= high){
+    // sort(nums.begin(), nums.end(), greater<int>());
+    // return nums[k-1];
+    int low = 0, high = nums.size() - 1;
+    while (low <= high) {
         int m = partion(nums, low, high);
-        if(m == nums.size() - k)
-            return nums[m];
-        else if(m > nums.size() - k)
+        if (m == nums.size() - k)
+            return nums[ m ];
+        else if (m > nums.size() - k)
             high = m - 1;
         else
             low = m + 1;
@@ -35,10 +35,10 @@ int findKthLargest(vector<int> &nums, int k) {
 }
 
 int main() {
-	vector<int> nums = {4,1,2};
-	findKthLargest(nums, 1);
-	for(auto i: nums) {
-		cout<<i<<" ";
-	}
-	return 0;
+    vector<int> nums = {4, 1, 2};
+    findKthLargest(nums, 1);
+    for (auto i : nums) {
+        cout << i << " ";
+    }
+    return 0;
 }
--- array/378.kth_smallest_element_in_sorted_matrix.cc
+++ array/378.kth_smallest_element_in_sorted_matrix.cc
@@ -2,23 +2,24 @@
 #include <vector>
 using namespace std;
 
-int kthSmallest(vector<vector<int>>& matrix, int k) {
-    int n = matrix.size();
-   	int lo = matrix[0][0];
-   	int hi = matrix[n-1][n-1];
+int kthSmallest(vector<vector<int>> &matrix, int k) {
+    int n   = matrix.size();
+    int lo  = matrix[ 0 ][ 0 ];
+    int hi  = matrix[ n - 1 ][ n - 1 ];
     int cnt = 0;
-   	while(lo < hi) {
-   		int mid = (lo + hi)/2;
-   		for(int i = 0; i < n; i++) {
-   			int cnt = 0, j = n - 1;
-   			while(j >= 0 && matrix[i][j] > mid) j--;
-   			cnt += (j+1);
-   		}
-   		if (cnt < k) {
-   			lo = mid + 1;
-   		} else {
-   			hi = mid;
-   		}
-   	}
+    while (lo < hi) {
+        int mid = (lo + hi) / 2;
+        for (int i = 0; i < n; i++) {
+            int cnt = 0, j = n - 1;
+            while (j >= 0 && matrix[ i ][ j ] > mid)
+                j--;
+            cnt += (j + 1);
+        }
+        if (cnt < k) {
+            lo = mid + 1;
+        } else {
+            hi = mid;
+        }
+    }
     return lo;
 }
